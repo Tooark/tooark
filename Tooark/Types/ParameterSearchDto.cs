ce32abdd39a132f27a71c4a6607f20e868841a32
Paulo Freitas <paulosfjunior@gmail.com>
2024-01-24 11:49:16 -0300

    Adicionado descrição ao campos do parametro de busca

--- Tooark/Types/ParameterSearchDto.cs
+++ Tooark/Types/ParameterSearchDto.cs
@@ -1,19 +1,49 @@
 namespace Tooark.Types;
 
-public class ParameterSearchDto
+/// <summary>
+/// Classe para parâmetros de busca
+/// </summary>
+public abstract class ParameterSearchDto
 {
+  /// <summary>
+  /// Informação a ser procurada
+  /// </summary>
   public string? Search { get; set; }
+
+  /// <summary>
+  /// Índice da paginação. Por padrão o valor é: 0
+  /// </summary>
   public int PageIndex { get; set; } = 0;
+
+  /// <summary>
+  /// Tamanho da paginação. Por padrão o valor é: 50. Para ignorar tamanho passar parâmetro: 0
+  /// </summary>
   public int PageSize { get; set; } = 50;
 }
 
-public class ParameterSearchOrderDto : ParameterSearchDto
+/// <summary>
+/// Classe para parâmetros de busca com parâmetro de ordenação
+/// </summary>
+public abstract class ParameterSearchOrderDto : ParameterSearchDto
 {
+  /// <summary>
+  /// Parâmetros para ordenação
+  /// </summary>
   public Order? Order { get; set; }
 }
 
+/// <summary>
+/// Classe para parâmetros de ordenação
+/// </summary>
 public class Order
 {
+  /// <summary>
+  /// Nome da coluna da tabela para ser ordenada, caso não exista ordena por ID
+  /// </summary>
   public string Field { get; set; } = null!;
+
+  /// <summary>
+  /// Sentido da ordenação. Crescente=true ou Decrescente=false. Por padrão a ordenação é crescente
+  /// </summary>
   public bool Asc { get; set; } = true;
 }
