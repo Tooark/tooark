de8876fd3c6d1d00742a2a1aaff3d12535cf959e
Paulo Freitas <paulo.freitas@grupojacto.com.br>
2024-01-25 20:48:51 -0300

    EA-1044 Criado validadores de atributos para email e senha, criado value objects de email e password com validação e implementado testes para validadores de atributos e VOs

---
+++ Tooark/Attributes/PasswordComplexityAttribute.cs
@@ -0,0 +1,77 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Tooark.Attributes;
+
+/// <summary>
+/// Atributo de validação que verifica se uma senha atende a critérios específicos de complexidade.
+/// </summary>
+public class PasswordComplexityAttribute : ValidationAttribute
+{
+  private readonly bool useLowercase;
+  private readonly bool useUppercase;
+  private readonly bool useNumbers;
+  private readonly bool useSymbols;
+  private readonly int passwordLength;
+
+  /// <summary>
+  /// Inicializa uma nova instância da classe PasswordComplexityAttribute com os critérios de complexidade especificados.
+  /// </summary>
+  /// <param name="useLowercase">Exige carácter minúsculo. Padrão: true</param>
+  /// <param name="useUppercase">Exige carácter maiúsculo. Padrão: true</param>
+  /// <param name="useNumbers">Exige carácter numérico. Padrão: true</param>
+  /// <param name="useSymbols">Exige carácter especial. Padrão: true</param>
+  /// <param name="passwordLength">Tamanho mínimo da senha. Padrão: 8</param>
+  public PasswordComplexityAttribute(
+    bool useLowercase = true,
+    bool useUppercase = true,
+    bool useNumbers = true,
+    bool useSymbols = true,
+    int passwordLength = 8)
+  {
+    this.useLowercase = useLowercase;
+    this.useUppercase = useUppercase;
+    this.useNumbers = useNumbers;
+    this.useSymbols = useSymbols;
+    this.passwordLength = passwordLength;
+  }
+
+  /// <summary>
+  /// Valida se o valor fornecido atende aos critérios de complexidade da senha.
+  /// </summary>
+  /// <param name="value">O valor da senha a ser validado.</param>
+  /// <returns>Verdadeiro se a senha atender aos critérios de complexidade, falso caso contrário.</returns>
+  public override bool IsValid(object? value)
+  {
+    if (value == null)
+    {
+      ErrorMessage = "RequiredField;Password";
+      return false;
+    }
+
+    string password = value.ToString()!;
+
+    if (!CheckValid(password))
+    {
+      ErrorMessage = "InvalidField;Password";
+      return false;
+    }
+
+    return true;
+  }
+
+  /// <summary>
+  /// Verifica se a senha fornecida atende aos critérios de complexidade.
+  /// </summary>
+  /// <param name="password">A senha a ser verificada.</param>
+  /// <returns>Verdadeiro se a senha atender aos critérios, falso caso contrário.</returns>
+  private bool CheckValid(string password)
+  {
+    return !(
+      password.Length < passwordLength ||
+      useLowercase && !password.Any(char.IsLower) ||
+      useUppercase && !password.Any(char.IsUpper) ||
+      useNumbers && !password.Any(char.IsDigit) ||
+      useSymbols && !password.Any(ch => !char.IsLetterOrDigit(ch))
+    );
+  }
+}
---
+++ Tooark/ValueObjects/Email.cs
@@ -0,0 +1,64 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Tooark.ValueObjects;
+
+/// <summary>
+/// Representa um email válido.
+/// </summary>
+public partial class Email : ValueObject
+{
+  private readonly string _value;
+
+  /// <summary>
+  /// Obtém o valor do email.
+  /// </summary>
+  public string Value { get => _value; }
+
+  /// <summary>
+  /// Inicializa uma nova instância da classe Email com o valor especificado.
+  /// </summary>
+  /// <param name="value">O valor do email a ser validado.</param>
+  public Email(string value)
+  {
+
+    if (string.IsNullOrWhiteSpace(value) || !new EmailAddressAttribute().IsValid(value) || !IsValidEmail(value))
+    {
+      throw new ArgumentException("InvalidField;Email");
+    }
+
+    _value = value;
+  }
+
+  /// <summary>
+  /// Valida o formato do email.
+  /// </summary>
+  /// <param name="email">O email a ser validado.</param>
+  /// <returns>Verdadeiro se o email for válido, falso caso contrário.</returns>
+  private static bool IsValidEmail(string email)
+  {
+    // Utilize uma expressão regular para validar o formato do email.
+    var regex = EmailRegex();
+    return regex.IsMatch(email);
+  }
+
+  [GeneratedRegex(
+    @"^[a-zA-Z0-9_.-]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9_.-]+[^.]$",
+    RegexOptions.IgnoreCase,
+    matchTimeoutMilliseconds: 250)]
+  private static partial Regex EmailRegex();
+
+  /// <summary>
+  /// Define uma conversão implícita de um objeto Email para uma string.
+  /// </summary>
+  /// <param name="email">O objeto Email a ser convertido.</param>
+  /// <returns>A string que representa o valor do email.</returns>
+  public static implicit operator string(Email email) => email._value;
+
+  /// <summary>
+  /// Define uma conversão implícita de uma string para um objeto Email.
+  /// </summary>
+  /// <param name="value">A string a ser convertida em um objeto Email.</param>
+  /// <returns>O objeto Email criado a partir da string fornecida.</returns>
+  public static implicit operator Email(string value) => new(value);
+}
