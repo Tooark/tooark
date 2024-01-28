7f2bd6400948e6a7673d0b99a8c350db780801c5
Paulo Freitas <paulosfjunior@gmail.com>
2024-01-28 10:45:50 -0300

    EA-1076 Revisado regex para validar email, implementado mais testes de validação de email

--- Tooark.Tests/ValueObjects/EmailTests.cs
+++ Tooark.Tests/ValueObjects/EmailTests.cs
@@ -4,14 +4,16 @@ namespace Tooark.Tests.ValueObjects;
 
 public class EmailTests
 {
-  [Fact]
-  public void Constructor_ValidEmail_ShouldCreateEmail()
+  [Theory]
+  [InlineData("teste@example.com")] // Email válido
+  [InlineData("te.te@example.com")] // Email válido
+  [InlineData("te-te@example.com")] // Email válido
+  [InlineData("te_te@example.com")] // Email válido
+  [InlineData("teste@exa-ple.com")] // Email válido
+  public void Constructor_ValidEmail_ShouldCreateEmail(string validEmail)
   {
-    // Arrange
-    var validEmail = "test@example.com";
-
     // Act
-    Email email = new (validEmail);
+    Email email = new(validEmail);
 
     // Assert
     Assert.Equal(validEmail, email.Value);
@@ -19,13 +21,41 @@ public class EmailTests
   }
 
   [Theory]
-  [InlineData("test@@example.com")] // Email mais de um @
-  [InlineData("test@example@.com")] // Email mais de um @
-  [InlineData("test@example.com.")] // Email terminado com .
-  [InlineData("test@example")] // Email sem domínio
-  [InlineData("test@com.")] // Email terminado com .
-  [InlineData("test@.com")] // Email com domínio inválido
-  [InlineData("test@")] // Email com domínio inválido
+  
+  [InlineData(".test@example.com")] // Email iniciado com .
+  [InlineData("-test@example.com")] // Email iniciado com -
+  [InlineData("_test@example.com")] // Email iniciado com _
+  [InlineData("test.@example.com")] // Email terminado com .
+  [InlineData("test-@example.com")] // Email terminado com -
+  [InlineData("test_@example.com")] // Email terminado com _
+  [InlineData("te!st@example.com")] // Email que carácter especial !
+  [InlineData("te#st@example.com")] // Email que carácter especial #
+  [InlineData("te$st@example.com")] // Email que carácter especial $
+  [InlineData("teçst@example.com")] // Email que carácter especial ç
+  [InlineData("test@@example.com")] // Email com mais de um @
+  [InlineData(".@example.com")] // Email com apenas .
+  [InlineData("teste@.")] // Domínio com apenas .
+  [InlineData("test@example@.com")] // Domínio com @
+  [InlineData("test@.example.com")] // Domínio iniciado com .
+  [InlineData("test@-example.com")] // Domínio iniciado com -
+  [InlineData("test@_example.com")] // Domínio iniciado com _
+  [InlineData("test@example-.com")] // Domínio terminado com -
+  [InlineData("test@example_.com")] // Domínio terminado com _
+  [InlineData("teste@exa!ple.com")] // Domínio que carácter especial !
+  [InlineData("teste@exa#ple.com")] // Domínio que carácter especial #
+  [InlineData("teste@exa$ple.com")] // Domínio que carácter especial $
+  [InlineData("teste@exaçple.com")] // Domínio que carácter especial ç
+  [InlineData("test@example.-com")] // Domínio iniciado com -
+  [InlineData("test@example._com")] // Domínio iniciado com _
+  [InlineData("test@example.com-")] // Domínio terminado com -
+  [InlineData("test@example.com_")] // Domínio terminado com _
+  [InlineData("test@example.com.")] // Domínio terminado com .
+  [InlineData("test@exaple..com")] // Domínio com dois .
+  [InlineData("test@example")] // Domínio sem extensão
+  [InlineData("test@.com")] // Domínio ausente apenas extensão
+  [InlineData("test@")] // Domínio ausente
+  [InlineData("")] // Email vazio
+  [InlineData(null)] // Email nulo
   public void Constructor_InvalidEmail_ShouldThrowArgumentException(string email)
   {
     // Arrange
