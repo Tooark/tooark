786640dc9fa52874233e132961c3b37a1a0ed6d0
Paulo Freitas <paulosfjunior@gmail.com>
2024-02-01 14:24:08 -0300

    EA-875 Modificado estrutura de string extension para ocultar codigo quando utilizado

--- Tooark/Extensions/StringExtensions.cs
+++ Tooark/Extensions/StringExtensions.cs
@@ -5,7 +5,7 @@ namespace Tooark.Extensions
   /// <summary>
   /// Métodos de extensão para a classe String.
   /// </summary>
-  public static partial class StringExtensions
+  public static class StringExtensions
   {
     /// <summary>
     /// Normaliza uma string removendo espaços e substituindo caracteres acentuados por seus equivalentes não acentuados.
@@ -13,6 +13,34 @@ namespace Tooark.Extensions
     /// <param name="value">A string a ser normalizada.</param>
     /// <returns>Uma string normalizada.</returns>
     public static string ToNormalize(this string value)
+    {
+      // Chama o método interno para normalizar a string
+      return InternalString.ToNormalize(value);
+    }
+
+    /// <summary>
+    /// Normaliza uma string usando expressões regulares para remover espaços e substituir caracteres acentuados.
+    /// </summary>
+    /// <param name="value">A string a ser normalizada.</param>
+    /// <returns>Uma string normalizada.</returns>
+    public static string ToNormalizeRegex(this string value)
+    {
+      // Chama o método interno para normalizar a string usando regex
+      return InternalString.ToNormalizeRegex(value);
+    }
+  }
+
+  /// <summary>
+  /// Métodos de extensão para a classe String.
+  /// </summary>
+  internal static partial class InternalString
+  {
+    /// <summary>
+    /// Normaliza uma string removendo espaços e substituindo caracteres acentuados por seus equivalentes não acentuados.
+    /// </summary>
+    /// <param name="value">A string a ser normalizada.</param>
+    /// <returns>Uma string normalizada.</returns>
+    internal static string ToNormalize(this string value)
     {
       if (value == null)
       {
@@ -83,7 +111,7 @@ namespace Tooark.Extensions
     /// </summary>
     /// <param name="value">A string a ser normalizada.</param>
     /// <returns>Uma string normalizada.</returns>
-    public static string ToNormalizeRegex(this string value)
+    internal static string ToNormalizeRegex(this string value)
     {
       if (value == null)
       {
