636d2394f2b6b82332003c9975efe8968d4921a4
Paulo Freitas <paulo.freitas@grupojacto.com.br>
2024-02-01 21:48:11 -0300

    EA-1135 Criado estrutura de Mapping para BaseEntity e DetailedEntity

---
+++ Tooark/Entities/Extension/BaseContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Tooark.Entities.Mapping;
+
+namespace Tooark.Entities.Extension;
+
+/// <summary>
+/// Classe BaseContext que estende de DbContext.
+/// Esta classe é usada para configurar o contexto do banco de dados com os mapeamentos
+/// específicos das entidades do modelo. 
+/// </summary>
+public class BaseContext : DbContext
+{
+  /// <summary>
+  /// Sobrescreve o método OnModelCreating para configurar o modelo de entidades.
+  /// Este método é chamado durante a inicialização do contexto para configurar o modelo
+  /// de banco de dados e seus mapeamentos.
+  /// </summary>
+  /// <param name="modelBuilder">Construtor de modelos para configurar entidades.</param>
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    // Aplica as configurações de mapeamento para a BaseEntity.
+    modelBuilder.ApplyConfiguration(new BaseEntityMap());
+
+    // Aplica as configurações de mapeamento para a DetailedEntity.
+    modelBuilder.ApplyConfiguration(new DetailedEntityMap());
+  }
+}
