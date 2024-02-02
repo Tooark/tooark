d8e930710330e1d3bdfe118b626b808e9a28e4ad
Paulo Freitas <paulosfjunior@gmail.com>
2024-02-02 00:00:43 -0300

    EA-1135 Criado cenarios de testes para validar mapping e context

--- Tooark/Entities/Context/BaseContext.cs
+++ Tooark/Entities/Context/BaseContext.cs
@@ -1,14 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Tooark.Entities.Mapping;
 
-namespace Tooark.Entities.Extension;
+namespace Tooark.Entities.Context;
 
 /// <summary>
 /// Classe BaseContext que estende de DbContext.
 /// Esta classe é usada para configurar o contexto do banco de dados com os mapeamentos
 /// específicos das entidades do modelo. 
 /// </summary>
-public class BaseContext : DbContext
+public class BaseContext(DbContextOptions<BaseContext> options) : DbContext(options)
 {
   /// <summary>
   /// Sobrescreve o método OnModelCreating para configurar o modelo de entidades.
