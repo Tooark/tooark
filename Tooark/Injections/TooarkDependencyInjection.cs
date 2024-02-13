d17bd220c0a913638974adeb87ac5794414a999e
Paulo Freitas <paulo.freitas@grupojacto.com.br>
2024-02-13 18:26:26 -0300

    Criado injeção de dependencias para o tooark separado o httpclientservice da interface e factory

---
+++ Tooark/Injections/TooarkDependencyInjection.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using Tooark.Services;
+using Tooark.Services.Interface;
+
+namespace Tooark.Injections;
+
+/// <summary>
+/// Classe de extensão para IServiceCollection para adicionar serviços do Tooark ao contêiner de injeção de dependências.
+/// </summary>
+public static class TooarkDependencyInjection
+{
+  /// <summary>
+  /// Adiciona os serviços do Tooark ao IServiceCollection.
+  /// </summary>
+  /// <param name="services">O IServiceCollection ao qual os serviços serão adicionados.</param>
+  /// <returns>O IServiceCollection com os serviços do Tooark adicionados.</returns>
+  public static IServiceCollection AddTooarkServices(this IServiceCollection services)
+  {
+    // Registra o HttpClientService para ser usado com a interface IHttpClientService
+    services.AddHttpClient<IHttpClientService, HttpClientService>();
+
+    return services;
+  }
+}
