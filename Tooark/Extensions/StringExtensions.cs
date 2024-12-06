b99a48712d04a8cc1e85ba162dc4558a4204e208
Paulo Freitas <paulosfjunior@gmail.com>
2024-12-06 07:47:06 -0300

    Adicionado métodos  e testes para conversão de SnakeCase para PascalCase e melhorias na normalização de strings

--- Tooark/Extensions/StringExtensions.cs
+++ Tooark/Extensions/StringExtensions.cs
@@ -28,6 +28,17 @@ public static class StringExtensions
     // Chama o método interno para normalizar a string usando regex
     return InternalString.ToNormalizeRegex(value);
   }
+
+  /// <summary>
+  /// Converte uma string de snake_case para PascalCase.
+  /// </summary>
+  /// <param name="value">A string em snake_case.</param>
+  /// <returns>A string convertida para PascalCase.</returns>
+  public static string ToPascalCase(this string value)
+  {
+    // Chama o método interno para converter a string para PascalCase
+    return InternalString.ToPascalCase(value);
+  }
 }
 
 /// <summary>
@@ -53,5 +64,22 @@ internal static partial class InternalString
   internal static string ToNormalizeRegex(this string value)
   {
     return NormalizeValueRegex(value);
+  }
+
+  /// <summary>
+  /// Converte uma string de snake_case para PascalCase.
+  /// </summary>
+  /// <param name="value">A string em snake_case.</param>
+  /// <returns>A string convertida para PascalCase.</returns>
+  internal static string ToPascalCase(this string value)
+  {
+    // Verifica se a string é nula ou vazia
+    if (string.IsNullOrEmpty(value))
+    {
+      return value;
+    }
+
+    // Converte a string para PascalCase
+    return string.Concat(value.Split('_').Where(word => !string.IsNullOrEmpty(word)).Select(word => char.ToUpper(word[0]) + word[1..]));
   }
 }
