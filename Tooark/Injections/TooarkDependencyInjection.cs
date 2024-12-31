0e7ea5d6978f8c58ff6afdd2fb97a5be6d0b717f
Paulo Freitas <paulosfjunior@gmail.com>
2024-12-30 22:52:19 -0300

    SINT-1811 Adiciona classes de injeção de dependência para serviços de armazenamento, HttpClient e localizador de strings; atualiza a configuração de serviços na classe TooarkDependencyInjection.

--- Tooark/Injections/TooarkDependencyInjection.cs
+++ Tooark/Injections/TooarkDependencyInjection.cs
@@ -1,93 +1,35 @@
-using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Localization;
-using Tooark.Exceptions;
-using Tooark.Extensions;
-using Tooark.Factories;
-using Tooark.Interfaces;
+using Tooark.Options;
 
 namespace Tooark.Injections;
 
 /// <summary>
 /// Classe de extensão para configurar e adicionar serviços específicos da Tooark ao contêiner de injeção de dependência.
 /// </summary>
-public static class TooarkDependencyInjection
+public static partial class TooarkDependencyInjection
 {
   /// <summary>
   /// Adiciona e configura os serviços da Tooark, incluindo serviços HTTP e RabbitMQ.
   /// </summary>
   /// <param name="services">A coleção de serviços para adicionar os serviços da Tooark.</param>
-  /// <param name="resourceAdditionalPaths">Dicionário com caminhos adicionais para arquivos JSON de recursos.</param>
+  /// <param name="configuration">A configuração da aplicação.</param>
+  /// <param name="localizerOptions">Configurações para o serviço de localização de recursos. Parâmetro opcional.</param>
   /// <returns>A coleção de serviços com os serviços da Tooark adicionados.</returns>
-  public static IServiceCollection AddTooarkServices(this IServiceCollection services, Dictionary<string, string>? resourceAdditionalPaths = null)
+  public static IServiceCollection AddTooarkServices(
+    this IServiceCollection services,
+    IConfiguration? configuration = null,
+    LocalizerOptions? localizerOptions = null
+  )
   {
     services.AddHttpClientService();
-    services.AddJsonStringLocalizer(resourceAdditionalPaths);
-    services.AddAppExceptionLocalizer();
-    services.AddStorageService();
+    services.AddJsonStringLocalizer(localizerOptions);
+    services.AddAppException();
 
-    return services;
-  }
-
-  /// <summary>
-  /// Adiciona o serviço HttpClient ao contêiner de injeção de dependência.
-  /// </summary>
-  /// <param name="services">A coleção de serviços para adicionar o serviço HttpClient.</param>
-  /// <returns>A coleção de serviços com o serviço HttpClient adicionado.</returns>
-  public static IServiceCollection AddHttpClientService(this IServiceCollection services)
-  {
-    services.AddHttpClient();
-
-    services.AddTransient<IHttpClientService>(provider =>
+    if(configuration is not null)
     {
-      var httpClientFactory = provider.GetRequiredService<IHttpClientFactory>();
-      var httpClient = httpClientFactory.CreateClient();
-
-      return HttpClientServiceFactory.Create(httpClient);
-    });
-
-    return services;
-  }
-
-  /// <summary>
-  /// Adiciona o serviço JsonStringLocalizer ao contêiner de injeção de dependência.
-  /// </summary>
-  /// <param name="services">A coleção de serviços para adicionar o serviço JsonStringLocalizer.</param>
-  /// /// <param name="resourceAdditionalPaths">Dicionário com caminhos adicionais para arquivos JSON de recursos.</param>
-  /// <returns>A coleção de serviços com o serviço JsonStringLocalizer adicionado.</returns>
-  public static IServiceCollection AddJsonStringLocalizer(this IServiceCollection services, Dictionary<string, string>? resourceAdditionalPaths = null)
-  {
-    services.AddSingleton<IStringLocalizerFactory, JsonStringLocalizerFactory>();
-    services.AddSingleton<IStringLocalizerFactory>(provider =>
-      new JsonStringLocalizerFactory(
-        provider.GetRequiredService<IDistributedCache>(),
-        resourceAdditionalPaths
-      )
-    );
-
-    services.AddTransient<IStringLocalizer>(provider =>
-    {
-      var factory = provider.GetRequiredService<IStringLocalizerFactory>();
-
-      return factory.Create(typeof(JsonStringLocalizerExtension));
-    });
-
-    return services;
-  }
-
-  /// <summary>
-  /// Adiciona o serviço AppExceptionLocalizer ao contêiner de injeção de dependência.
-  /// </summary>
-  /// <param name="services">A coleção de serviços para adicionar o serviço AppExceptionLocalizer.</param>
-  /// <returns>A coleção de serviços com o serviço AppExceptionLocalizer adicionado.</returns>
-  public static IServiceCollection AddAppExceptionLocalizer(this IServiceCollection services)
-  {
-    services.AddSingleton<IStringLocalizer<AppException>>(provider =>
-    {
-      var factory = provider.GetRequiredService<IStringLocalizerFactory>();
-
-      return (IStringLocalizer<AppException>)factory.Create(typeof(AppException));
-    });
+      services.AddBucketService(configuration);
+    }
 
     return services;
   }
