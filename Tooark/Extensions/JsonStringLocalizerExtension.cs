6b32acfc3d160bffc9da479fa17ebc0ead35c14f
Paulo Freitas <paulosfjunior@gmail.com>
2025-01-17 01:38:44 -0300

    Ajusta tipos de parâmetros e valores de retorno em testes de normalização de string; corrige valor de caminho físico em MockFileInfo e remove referência desnecessária em JsonStringLocalizerExtension.

--- Tooark/Extensions/JsonStringLocalizerExtension.cs
+++ Tooark/Extensions/JsonStringLocalizerExtension.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text.Json;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Localization;
 using static Tooark.Utils.Util;
 
