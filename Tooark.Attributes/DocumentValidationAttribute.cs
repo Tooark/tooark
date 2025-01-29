ace4b935fb2285c644797937b8bd7bf9c5e04688
Paulo Freitas <paulo.freitas@grupojacto.com.br>
2025-01-29 09:23:53 -0300

    Adiciona atributos de validação para email, código postals documentos e senha, removendo atributos obsoletos e melhorando a estrutura do projeto.

---
+++ Tooark.Attributes/DocumentValidationAttribute.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Tooark.Enums;
+
+namespace Tooark.Attributes;
+
+/// <summary>
+/// Atributo de validação de documento.
+/// </summary>
+/// <remarks>
+/// O documento é validado utilizando uma expressão regular.
+/// </remarks>
+/// <param name="type">Tipo de documento a ser validado.</param>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public partial class DocumentValidationAttribute(EDocumentType type) : ValidationAttribute
+{
+  /// <summary>
+  /// Sobrescreve o método de validação para verificar se o valor é um documento válido.
+  /// </summary>
+  /// <param name="value">O objeto a ser validado.</param>
+  /// <returns>Retornar verdadeiro se o valor for um documento válido.</returns>
+  public override bool IsValid(object? value)
+  {
+    // Converta o valor para uma string.
+    string? document = value?.ToString();
+
+    // Verifique se o valor é nulo.
+    if (string.IsNullOrEmpty(document))
+    {
+      // Defina a mensagem de erro padrão.
+      ErrorMessage = "Field.Required;Document";
+
+      // Retorne falso.
+      return false;
+    }
+
+    // Verifique se o valor é um document válido.
+    if (
+      !Regex.IsMatch(document, type.ToRegex(), RegexOptions.None, TimeSpan.FromMilliseconds(300)) ||
+      !type.IsValid(document)
+    )
+    {
+      // Defina a mensagem de erro padrão.
+      ErrorMessage = "Field.Invalid;Document";
+
+      // Retorne falso.
+      return false;
+    }
+
+    // Retorne verdadeiro.
+    return true;
+  }
+}
---
+++ Tooark.Attributes/ZipCodeValidationAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Tooark.Validations.Patterns;
+
+namespace Tooark.Attributes;
+
+/// <summary>
+/// Atributo de validação de código postal.
+/// </summary>
+/// <remarks>
+/// O código postal é validado utilizando uma expressão regular.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public partial class ZipCodeValidationAttribute : ValidationAttribute
+{
+
+  /// <summary>
+  /// Sobrescreve o método de validação para verificar se o valor é um código postal válido.
+  /// </summary>
+  /// <param name="value">O objeto a ser validado.</param>
+  /// <returns>Retornar verdadeiro se o valor for um código postal válido.</returns>
+  public override bool IsValid(object? value)
+  {
+    // Converta o valor para uma string.
+    string? zipCode = value?.ToString();
+
+    // Verifique se o valor é nulo.
+    if (string.IsNullOrEmpty(zipCode))
+    {
+      // Defina a mensagem de erro padrão.
+      ErrorMessage = "Field.Required;ZipCode";
+
+      // Retorne falso.
+      return false;
+    }
+
+    // Verifique se o valor é um código postal válido.
+    if (!Regex.IsMatch(zipCode, RegexPattern.ZipCode, RegexOptions.None, TimeSpan.FromMilliseconds(300)))
+    {
+      // Defina a mensagem de erro padrão.
+      ErrorMessage = "Field.Invalid;ZipCode";
+
+      // Retorne falso.
+      return false;
+    }
+
+    // Retorne verdadeiro.
+    return true;
+  }
+}
