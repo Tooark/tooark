0ef2a958702bc70c44cae431ee9ad6f7afd6ea8d
Paulo Freitas <paulo.freitas@grupojacto.com.br>
2025-02-13 23:03:45 -0300

    Revisando estrutura do JsonStringLocalizer, para não receber pasta adicional, apenas lendo arquivo de idioma dentro do Resources, excluido options e value objects do extensions, revisado testes

--- Tooark.Extensions/Factories/JsonStringLocalizerFactory.cs
+++ Tooark.Extensions/Factories/JsonStringLocalizerFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Localization;
-using Tooark.Extensions.ValueObjects;
 
 namespace Tooark.Extensions.Factories;
 
@@ -11,39 +10,21 @@ namespace Tooark.Extensions.Factories;
 /// A fábrica é responsável por criar instâncias de <see cref="JsonStringLocalizerExtension"/>.
 /// </remarks>
 /// <param name="distributedCache">Cache distribuído para armazenar as traduções.</param>
-/// <param name="additionalResourcePath">Caminhos adicionais para buscar os arquivos de tradução. Parâmetro opcional.</param>
-/// <param name="additionalResourceStream">Stream do arquivo de tradução. Parâmetro opcional.</param>
 /// <seealso cref="IStringLocalizerFactory"/>
-public class JsonStringLocalizerFactory(
-  IDistributedCache distributedCache,
-  IList<ResourcePath>? additionalResourcePath = null,
-  IList<ResourceStream>? additionalResourceStream = null
-) : IStringLocalizerFactory
+public class JsonStringLocalizerFactory(IDistributedCache distributedCache) : IStringLocalizerFactory
 {
   /// <summary>
   /// Cache distribuído para armazenar as traduções.
   /// </summary>
   private readonly IDistributedCache _distributedCache = distributedCache;
 
-  /// <summary>
-  /// Caminhos adicionais para buscar os arquivos de tradução.
-  /// </summary>
-  private readonly IList<ResourcePath>? _additionalResourcePath = additionalResourcePath;
-
-  /// <summary>
-  /// Stream do arquivo de tradução.
-  /// </summary>
-  private readonly IList<ResourceStream>? _additionalResourceStream = additionalResourceStream;
 
   /// <summary>
   /// Cria uma instância de <see cref="JsonStringLocalizerExtension"/>.
   /// </summary>
   /// <param name="resourceSource">Parâmetro não utilizado.</param>	
   /// <returns>Instância de <see cref="IStringLocalizer"/>.</returns>
-  public IStringLocalizer Create(Type resourceSource)
-  {
-    return new JsonStringLocalizerExtension(_distributedCache, _additionalResourcePath, _additionalResourceStream);
-  }
+  public IStringLocalizer Create(Type resourceSource) => new JsonStringLocalizerExtension(_distributedCache);
 
   /// <summary>
   /// Cria uma instância de <see cref="JsonStringLocalizerExtension"/>.
@@ -51,8 +32,5 @@ public class JsonStringLocalizerFactory(
   /// <param name="baseName">Parâmetro não utilizado.</param>
   /// <param name="location">Parâmetro não utilizado.</param>
   /// <returns>Instância de <see cref="IStringLocalizer"/>.</returns>
-  public IStringLocalizer Create(string baseName, string location)
-  {
-    return new JsonStringLocalizerExtension(_distributedCache, _additionalResourcePath, _additionalResourceStream);
-  }
+  public IStringLocalizer Create(string baseName, string location) => new JsonStringLocalizerExtension(_distributedCache);
 }
