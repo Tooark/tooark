c460284d03ec1226282f4c0b0a6ed13af01495ad
Paulo Freitas <paulosfjunior@gmail.com>
2025-03-01 21:33:49 -0300

    Adiciona atributo de validação para links de vídeo com suporte a YouTube, Vimeo e Dailymotion

---
+++ Tooark.Validations/LinkVideoValidation.cs
@@ -0,0 +1,94 @@
+﻿using Tooark.Validations.Messages;
+using Tooark.Validations.Patterns;
+
+namespace Tooark.Validations;
+
+/// <summary>
+/// Classe de validação de Protocol.
+/// </summary>
+public partial class Validation
+{
+  #region IsLinkVideo
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato de um link de vídeo. Com mensagem padrão.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideo(string value, string property) =>
+    IsLinkVideo(value, property, ValidationErrorMessages.IsValid(property, "Url"));
+
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato de um link de vídeo.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <param name="message">Mensagem de erro.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideo(string value, string property, string message) =>
+    Match(value, RegexPattern.LinkVideo, property, message);
+  #endregion
+
+  #region IsLinkVideoYouTube
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato link de vídeo do YouTube. Com mensagem padrão.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideoYouTube(string value, string property) =>
+    IsLinkVideoYouTube(value, property, ValidationErrorMessages.IsValid(property, "Ftp"));
+
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato link de vídeo do YouTube.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <param name="message">Mensagem de erro.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideoYouTube(string value, string property, string message) =>
+    Match(value, RegexPattern.YouTube, property, message);
+  #endregion
+
+  #region IsLinkVideoVimeo
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato link de vídeo do Vimeo. Com mensagem padrão.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideoVimeo(string value, string property) =>
+    IsLinkVideoVimeo(value, property, ValidationErrorMessages.IsValid(property, "Sftp"));
+
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato link de vídeo do Vimeo.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <param name="message">Mensagem de erro.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideoVimeo(string value, string property, string message) =>
+    Match(value, RegexPattern.Vimeo, property, message);
+  #endregion
+
+  #region IsLinkVideoDailymotion
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato link de vídeo do Dailymotion. Com mensagem padrão.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideoDailymotion(string value, string property) =>
+    IsLinkVideoDailymotion(value, property, ValidationErrorMessages.IsValid(property, "ProtocolFtp"));
+
+  /// <summary>
+  /// Verifica se o valor corresponde ao formato link de vídeo do Dailymotion.
+  /// </summary>
+  /// <param name="value">Valor a ser validado.</param>
+  /// <param name="property">Propriedade a ser validada.</param>
+  /// <param name="message">Mensagem de erro.</param>
+  /// <returns>Validação.</returns>
+  public Validation IsLinkVideoDailymotion(string value, string property, string message) =>
+    Match(value, RegexPattern.Dailymotion, property, message);
+  #endregion
+}
