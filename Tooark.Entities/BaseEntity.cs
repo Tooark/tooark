fa097061187d8be5801eb20fd6ec1e9fb24aa270
Paulo Freitas <paulosfjunior@gmail.com>
2025-04-22 13:02:35 -0300

    Atualiza versão para 2.0.7 e adiciona notas de lançamento; remove importação desnecessária em BaseEntity.

--- Tooark.Entities/BaseEntity.cs
+++ Tooark.Entities/BaseEntity.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Tooark.Notifications;
-using Tooark.Validations;
 
 namespace Tooark.Entities;
 
